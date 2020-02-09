7200810e8ea0250f4ccb2e06ba67079371bdee58
Harvey <harveyjwilliams@aol.co.uk>
2020-02-09 01:46:57 +0100

    Changes for Custom Start Page.

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,35 +1,29 @@
-var data_links = "links.json";
-  
-$(document).ready(function(){
-  $.getJSON(data_links,
-    function (data) {
+new CustomStartStorage().get()
+  .then(data => {
+    $(document).ready(function(){
       var mysource = $('#links-template').html();
       var mytemplate = Handlebars.compile(mysource);
       var myresult = mytemplate(data)
       $('#links').html(myresult);
-  });
-});
+    });
 
-var data_apps = "apps.json";
-  
-$(document).ready(function(){
-  $.getJSON(data_apps,
-    function (data) {
+    $(document).ready(function(){
       var mysource = $('#apps-template').html();
       var mytemplate = Handlebars.compile(mysource);
       var myresult = mytemplate(data)
       $('#apps').html(myresult);
-  });
-});
+    });
 
-var data_providers = "providers.json";
-  
-$(document).ready(function(){
-  $.getJSON(data_providers,
-    function (data) {
-      var mysource = $('#providers-template').html();
-      var mytemplate = Handlebars.compile(mysource);
-      var myresult = mytemplate(data)
-      $('#providers').html(myresult);
+    var data_providers = "providers.json";
+
+    $(document).ready(function(){
+      $.getJSON(data_providers,
+        function (data) {
+          var mysource = $('#providers-template').html();
+          var mytemplate = Handlebars.compile(mysource);
+          var myresult = mytemplate(data)
+          $('#providers').html(myresult);
+      });
+    });
   });
-});
+
